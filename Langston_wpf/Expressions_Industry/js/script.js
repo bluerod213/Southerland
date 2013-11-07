dfa78c18e3f3fe7c7da61a60b85156b6f4ccaff0
bluerod213 <Southlds@fullsail.edu>
2013-11-07 18:19:10 -0500

    Expressions_Industry
    11th commit

---
+++ Langston_wpf/Expressions_Industry/js/script.js
@@ -0,0 +1,11 @@
+//Langston Southerland 11/6/13 Expressions - Industry
+
+//Calculating the amount of content stored in computer software with the number of CD's from each room in the office building.
+
+//Damaged disks that are located in the third room are removed
+
+var computerDisk = ["First Room", "Second Room", "Third Room", "Fourth Room"]; //The array that holds all the rooms from the office 
+
+
+
+	
