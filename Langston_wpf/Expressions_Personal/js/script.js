70c4e98e2ee69b6716a000aacc58807f035254b5
bluerod213 <Southlds@fullsail.edu>
2013-11-07 16:18:09 -0500

    Expressions_Personal
    1st commit

---
+++ Langston_wpf/Expressions_Personal/js/script.js
@@ -0,0 +1,11 @@
+//Langston Southerland 11/6/13 Personal - Expressions
+
+//Calculating the total of the area of my newly built rectangle kitchen floor and my square living room floor
+
+
+var area1 = 40;   //Declaring value to the variable 
+
+
+
+
+
