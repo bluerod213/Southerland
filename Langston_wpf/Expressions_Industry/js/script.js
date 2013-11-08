607ce607f5fdc71807928b95f4d5718aadd56496
bluerod213 <Southlds@fullsail.edu>
2013-11-07 19:48:02 -0500

    Expressions_Industry
    14th commit

--- Langston_wpf/Expressions_Industry/js/script.js
+++ Langston_wpf/Expressions_Industry/js/script.js
@@ -4,14 +4,12 @@
 
 //Damaged disks that are located in the third room are removed
 
-var computerDisk = ["First Room", "Second Room", "Third Room", "Fourth Room"]; //The array that holds the number of office rooms 
-
+var computerDisk =  ["First Room", "Second Room", "Third Room", "Fourth Room"]; //The array that holds the number of office rooms 
 var data = [500] //Value for content on each CD
 
 var firstRoom = [20] * data;  //First room value of CD's
 var secondRoom = [25] * data;  //Second room value of CD's
-var thirdRoom = [50];  //Third room value of CD's
+var thirdRoom = [50];  //Third room value of CD's but not calculated with data 
 var fourthRoom = [12] * data; //Fourth room value of CD's
 
-
-
+console.log(computerDisk); //Placing Array in order
