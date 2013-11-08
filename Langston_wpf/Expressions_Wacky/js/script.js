bc454407ce29117fe45cc4398a356c2d21753454
bluerod213 <Southlds@fullsail.edu>
2013-11-07 23:48:55 -0500

    Expressions_Wacky
    18th commit

--- Langston_wpf/Expressions_Wacky/js/script.js
+++ Langston_wpf/Expressions_Wacky/js/script.js
@@ -7,3 +7,5 @@ var redLight = prompt("Enter 70 seconds for red light to function!"); //Time lim
 
 var yellowLight = prompt("Enter 5 seconds for yellow light to function!"); //Time limit aftter yellow light is activated
 
+var greenLight = prompt("Enter 65 seconds green light to function!") //Time limit after green light is activated
+
