68df32780d337f4ffa0ed809e3051b3898796f5c
bluerod213 <Southlds@fullsail.edu>
2013-11-07 23:45:21 -0500

    Expressions_Wacky
    16th commit

---
+++ Langston_wpf/Expressions_Wacky/js/script.js
@@ -0,0 +1,7 @@
+//Langston Southerland 11/6/13 Expressions - Wacky
+
+
+//Changing the color of traffic light from a computer machine by inputing a value 
+
+var redLight = prompt("Enter 70 seconds for red light to function!"); //Time limit after red light is activated
+
