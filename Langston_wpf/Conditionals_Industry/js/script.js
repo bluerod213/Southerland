72996b14d0aa19d99fe91400aa5f391b0b95b045
bluerod213 <Southlds@fullsail.edu>
2013-11-14 22:12:16 -0500

    Conditionals_Industry
    29th commit

---
+++ Langston_wpf/Conditionals_Industry/js/script.js
@@ -0,0 +1,6 @@
+//Langston Southerland 11/13/13 Conditonals - Industry
+
+//Determining which section of computer parts needed to repair moniter 
+
+
+var computerMoniter = 400;  //Parts of moniter that are currently in place
