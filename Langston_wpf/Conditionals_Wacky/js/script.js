389c16adf5102698bcb48cb26aad02e67caba709
bluerod213 <Southlds@fullsail.edu>
2013-11-14 23:53:22 -0500

    Conditionals_Wacky
    36th commit

--- Langston_wpf/Conditionals_Wacky/js/script.js
+++ Langston_wpf/Conditionals_Wacky/js/script.js
@@ -3,8 +3,9 @@
 //Did Jake steal the grapes from the farm Friday? 
      
 	 intGrapes = 500;    //500 equals value for total number of grapes
-	
-	 
+	 stolenGrapes = 5 * '' && 100 * 100 * 0;    //5 times the amount of the other value which is 0
+     
+
 	 
 	 
 	 
