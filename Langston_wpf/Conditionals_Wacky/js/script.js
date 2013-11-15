cc4176f846096bf05e15749978e17aae6c9f978a
bluerod213 <Southlds@fullsail.edu>
2013-11-14 23:59:19 -0500

    Conditionals_Wacky
    41st commit

--- Langston_wpf/Conditionals_Wacky/js/script.js
+++ Langston_wpf/Conditionals_Wacky/js/script.js
@@ -1,11 +1,11 @@
-////Langston 11/13/13 Conditionals - Wacky
-//
-////Did Jake steal the grapes from the farm Friday? 
-//     
-//	 intGrapes = 500;    //500 equals value for total number of grapes
-//	 stolenGrapes = 5 * '' && 100 * 100 * 0;    //5 times the amount of the other value which is 0
-//     
-//	 intGrapes = (stolenGrapes === 45 * 2) ? true : false;    //ternary operator determining whether Jake is stealing from farm
+//Langston 11/13/13 Conditionals - Wacky
+
+//Did Jake steal the grapes from the farm Friday? 
+     
+	 intGrapes = 500;    //500 equals value for total number of grapes
+	 stolenGrapes = 5 * '' && 100 * 100 * 0;    //5 times the amount of the other value which is 0
+     
+	 intGrapes = (stolenGrapes === 45 * 2) ? true : false;    //ternary operator determining whether Jake is stealing from farm
 	 
 	 prompt("Enter amount of grapes on the farm before Friday!");   //Message for entering grapes on Farm previously
 	 
