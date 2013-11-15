77197265cc7812a4a5db1ecb1cd78f5bf2de02a8
bluerod213 <Southlds@fullsail.edu>
2013-11-14 19:41:19 -0500

    Conditionals_Personal
    24th commit

--- Langston_wpf/Conditionals_Personal/js/script.js
+++ Langston_wpf/Conditionals_Personal/js/script.js
@@ -6,9 +6,6 @@ var workHour = 4;  // The fifth hour on the clock
 var hoursBusy = 8;        // Hours spent on project   
    
    if(workHour > 9){          //Stating if work hour is greater than nine O'clock 
-     ("You can go home.")
-   }
-
-
-
-
+     ("You can go home.")   //If the hour is greater than nine you can go home; false.
+}else if(workHour < 5){        //Else if the work hour is less than 5 which is 4
+  
