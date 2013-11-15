4069ca96497c0c3c9058c08d7f445a77374930ab
bluerod213 <Southlds@fullsail.edu>
2013-11-14 23:52:24 -0500

    Conditionals_Wacky
    35th commit

---
+++ Langston_wpf/Conditionals_Wacky/js/script.js
@@ -0,0 +1,26 @@
+//Langston 11/13/13 Conditionals - Wacky
+
+//Did Jake steal the grapes from the farm Friday? 
+     
+	 intGrapes = 500;    //500 equals value for total number of grapes
+	
+	 
+	 
+	 
+	 
+	
+
+	 
+	 
+	 
+     
+	
+
+ 
+
+
+
+
+
+
+
