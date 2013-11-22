6eeb33bfdf14baebfb0b8df2939a104f9c57c580
bluerod213 <Southlds@fullsail.edu>
2013-11-21 22:52:42 -0500

    Functions_Personal
    42nd commit

---
+++ Langston_wpf/Functions_Personal/js/script.js
@@ -0,0 +1,9 @@
+//Langston Southerland 11/20/13 Functions - Personal
+
+
+//Calculating number of paint needed for navy blue color in house that needs more paint using function
+
+
+function coloredRooms(){    //My function 
+
+
